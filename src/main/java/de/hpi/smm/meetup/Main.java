d2ff091dd515dc431e8a61dd026519cbac9248c6
jaeyoon.jung <jaeyoon.jung@student.hpi.uni-potsdam.de>
2014-12-05 11:02:42 +0100

    getKloutScore added

--- src/main/java/de/hpi/smm/meetup/Main.java
+++ src/main/java/de/hpi/smm/meetup/Main.java
@@ -2,17 +2,26 @@ package de.hpi.smm.meetup;
 
 import java.io.IOException;
 
+import org.json.JSONException;
+
 import de.hpi.smm.meetup.features.Compactness;
+import de.hpi.smm.meetup.features.Klout;
 
 
 public class Main 
 {
-    public static void main( String[] args ) throws IOException
+    public static void main( String[] args ) throws IOException, JSONException
     {
+    	// Compactness
         String text = "International Business Machines (IBM) has three ways to profit from corporate and government spending on technology — services, hardware, and software. And thanks to Big Data, Big Blue is poised to use all three to add millions to its bottom line. As he explained in an October 26 interview, Michael Schroeck, Global Information Management Leader, IBM Global Business Services, believes that IBM can help organizations using all three of those while tapping into a $64 billion market.";
         Compactness compactness = new Compactness(text);
         System.out.println("Compactness by Characters: " + compactness.getCompactnessByChars() + "%");
         System.out.println("Compactness by Words: " + compactness.getCompactnessByWords() + "%");
+        
+        // KloutScore
+        Klout klout = new Klout("barackobama");
+        double kloutScore = klout.getKloutScore();
+        System.out.println("Klout score of the twitter user @" + klout.getTwitterId() + " is " + kloutScore);
     }
     	
 }
---
+++ src/main/java/de/hpi/smm/meetup/features/Klout.java
@@ -0,0 +1,87 @@
+package de.hpi.smm.meetup.features;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Klout {
+
+	String twitterId;
+	String kloutId;
+	double kloutScore;
+	static String KloutAPIKey = "bse4cn98ht3xwjq63d9e2tte"; // Jaeyoon's Klout API Key
+															
+
+	public Klout(String twitterId) {
+		this.twitterId = twitterId;
+	}
+
+	public double getKloutScore() throws JSONException {
+		FindKloutId();
+		FindKloutScore();
+		return kloutScore;
+	}
+
+	private void FindKloutId() throws JSONException {
+		String url = "http://api.klout.com/v2/identity.json/twitter?screenName="
+				+ twitterId + "&key=" + KloutAPIKey;
+		String jsonString = callURL(url);
+		JSONObject obj = new JSONObject(jsonString);
+		String id = obj.getString("id");
+		this.kloutId = id;
+	}
+
+	private void FindKloutScore() throws JSONException {
+		String url = "http://api.klout.com/v2/user.json/" + kloutId
+				+ "/score?key=" + KloutAPIKey;
+		String jsonString = callURL(url);
+		JSONObject obj = new JSONObject(jsonString);
+		double score = obj.getDouble("score");
+		this.kloutScore = score;
+	}
+
+	private String callURL(String myURL) {
+		//System.out.println("Requested URL:" + myURL);
+		StringBuilder sb = new StringBuilder();
+		URLConnection urlConn = null;
+		InputStreamReader in = null;
+		try {
+			URL url = new URL(myURL);
+			urlConn = url.openConnection();
+			if (urlConn != null)
+				urlConn.setReadTimeout(60 * 1000);
+			if (urlConn != null && urlConn.getInputStream() != null) {
+				in = new InputStreamReader(urlConn.getInputStream(),
+						Charset.defaultCharset());
+				BufferedReader bufferedReader = new BufferedReader(in);
+				if (bufferedReader != null) {
+					int cp;
+					while ((cp = bufferedReader.read()) != -1) {
+						sb.append((char) cp);
+					}
+					bufferedReader.close();
+				}
+			}
+			in.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Exception while calling URL:" + myURL,
+					e);
+		}
+		return sb.toString();
+	}
+	
+	public String getTwitterId(){
+		return twitterId;
+	}
+	
+	public String getKloutId(){
+		return kloutId;
+	}
+	
+	
+}
