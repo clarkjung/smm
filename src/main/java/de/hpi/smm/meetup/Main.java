69833d9b47744da4be3ce1d819528d78b80f04a1
Georg Wiese <georgwiese@gmail.com>
2014-12-12 21:30:53 +0100

    Refactor: Split Main class by feature

--- src/main/java/de/hpi/smm/meetup/Main.java
+++ src/main/java/de/hpi/smm/meetup/Main.java
@@ -22,30 +22,14 @@ public class Main
 {
     public static void main( String[] args ) throws IOException, JSONException, ClassNotFoundException
     {
-//    	// Compactness
-//        String text = "International Business Machines (IBM) has three ways to profit from corporate and government spending on technology — services, hardware, and software. And thanks to Big Data, Big Blue is poised to use all three to add millions to its bottom line. As he explained in an October 26 interview, Michael Schroeck, Global Information Management Leader, IBM Global Business Services, believes that IBM can help organizations using all three of those while tapping into a $64 billion market.";
-//        Compactness compactness = new Compactness(text);
-//        System.out.println("Compactness by Characters: " + compactness.getCompactnessByChars() + "%");
-//        System.out.println("Compactness by Words: " + compactness.getCompactnessByWords() + "%");
-//        
-//        // KloutScore
-//        Klout klout = new Klout("barackobama");
-//        double kloutScore = klout.getKloutScore();
-//        System.out.println("Klout score of the twitter user @" + klout.getTwitterId() + " is " + kloutScore);
+
+
 //        
 //        // pos
 //        MaxentTagger tagger = new MaxentTagger("taggers/left3words-wsj-0-18.tagger");
 //        String sample = "she likes it";
 //        String tagged = tagger.tagString(sample);
 //        System.out.println(tagged);
-    
-    	String oldFilePath = "data/train/formal/";
-    	String newFilePath = "data/test/formal/";
-    	//String targetWordsPath = "target_words/";
-    	String trainFormalPathMac = "data/train/formal/";
-    	String trainInformalPathMac = "data/train/informal/";
-    	String targetWordsPath = "target_words/";
-    	String trainFormalPathWin = "data/train/formal/";
     	
 //    	MachineLearner machineLearner = new MachineLearner(trainFormalPathMac);
 //    	machineLearner.train("train_formal_2.txt");
@@ -71,12 +55,6 @@ public class Main
     	//double accuracyForFormal = machineLearner.getAccuracyForFormal();
     	//double accuracyForInformal = machineLearner.getAccuracyForInformal();
     	//System.out.println(accuracyForFormal + ", " + accuracyForInformal + ", " + machineLearner.getGeneralAccuracy());
-    	
-
-    	CrossValidation cv = new CrossValidation(trainFormalPathMac, trainInformalPathMac);
-    	MachineLearner machineLearner = new MachineLearner();
-    	machineLearner.trainAndTest(cv);
-    	System.out.println(machineLearner.getCVAccuracy());
     	
     }
     	
